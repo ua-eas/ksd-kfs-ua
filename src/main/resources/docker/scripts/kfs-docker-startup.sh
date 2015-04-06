a7dbf5623fc3d6efa55d7a973bf9558eab3c5666
rbtucker <rbtucker@email.arizona.edu>
2015-04-06 14:31:24 -0700

    cleaning up docker build

--- src/main/resources/docker/scripts/kfs-docker-startup.sh
+++ src/main/resources/docker/scripts/kfs-docker-startup.sh
@@ -4,9 +4,9 @@
 touch /tmp/kfs-startup.log
 
 # copy in tomcat platform configuration files
-cp $TOMCAT_CONFIGURATION_DIRECTORY/server.xml $TOMCAT_CONF_DIRECTORY
-cp $TOMCAT_CONFIGURATION_DIRECTORY/tomcat-users.xml $TOMCAT_CONF_DIRECTORY
-cp $TOMCAT_CONFIGURATION_DIRECTORY/tomcat7 $TOMCAT_STARTUP_CONFIG
+cp $TOMCAT_CONFIGURATION_DIRECTORY/server.xml /var/lib/tomcat7/conf
+cp $TOMCAT_CONFIGURATION_DIRECTORY/tomcat-users.xml /var/lib/tomcat7/conf
+cp $TOMCAT_CONFIGURATION_DIRECTORY/tomcat7 /etc/default/tomcat7
 
 # start required services
 /etc/init.d/tomcat7 start >> /tmp/kfs-startup.log
