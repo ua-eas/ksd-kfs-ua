d5f2931e2cff77ec8a4fcda29b2d494b072d610d
rbtucker <rbtucker@email.arizona.edu>
2015-04-07 10:21:32 -0700

    add some config examples

--- src/main/resources/docker/scripts/usr/local/bin/kfs-docker-startup.sh
+++ src/main/resources/docker/scripts/usr/local/bin/kfs-docker-startup.sh
@@ -6,7 +6,12 @@ touch /tmp/kfs-startup.log
 # copy in tomcat platform configuration files
 cp $TOMCAT_CONFIGURATION_DIRECTORY/server.xml /var/lib/tomcat7/conf
 cp $TOMCAT_CONFIGURATION_DIRECTORY/tomcat-users.xml /var/lib/tomcat7/conf
+cp $TOMCAT_CONFIGURATION_DIRECTORY/logging.properties /var/lib/tomcat7/conf
 cp $TOMCAT_CONFIGURATION_DIRECTORY/tomcat7 /etc/default/tomcat7
+cp $TOMCAT_CONFIGURATION_DIRECTORY/log4j.properties /var/lib/tomcat7/webapps/kfs/WEB-INF/classes
+
+mkdir -p $LOGS_DIRECTORY
+chmod 777 $LOGS_DIRECTORY
 
 # start required services
 /etc/init.d/tomcat7 start >> /tmp/kfs-startup.log
